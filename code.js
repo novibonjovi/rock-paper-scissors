4a3ecb2ddc256a1b8e21b3decbd02a359229cc32
novibonjovi <novak.nikolic92@gmail.com>
2020-05-18 22:51:01 +0200

    Game finished

---
+++ code.js
@@ -0,0 +1,52 @@
+let computerPlay = () => {
+  let choice = ['Rock', 'Paper', 'Scissors'];
+  return choice[Math.floor(Math.random() * choice.length)];
+};
+
+let playRound = (playerSelection, computerSelection) => {
+  if (
+    (playerSelection.toLowerCase() === 'rock' &&
+      computerSelection === 'Scissors') ||
+    (playerSelection.toLowerCase() === 'paper' &&
+      computerSelection === 'Rock') ||
+    (playerSelection.toLowerCase() === 'scissors' &&
+      computerSelection === 'Paper')
+  ) {
+    return `You Win! ${
+      playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    } beats ${computerSelection}`;
+  } else if (
+    (playerSelection.toLowerCase() === 'rock' &&
+      computerSelection === 'Paper') ||
+    (playerSelection.toLowerCase() === 'paper' &&
+      computerSelection === 'Scissors') ||
+    (playerSelection.toLowerCase() === 'scissors' &&
+      computerSelection === 'Rock')
+  ) {
+    return `You Lose! ${computerSelection} beats ${
+      playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    }`;
+  } else {
+    return "It's a Draw!";
+  }
+};
+
+let game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (i = 0; i < 5; i++) {
+    let playerSelection = prompt('Enter: Rock, Paper or Scissors', '');
+    let result = playRound(playerSelection, computerPlay());
+
+    if (result.indexOf('Win') > 0) {
+      playerScore++;
+    } else if (result.indexOf('Lose') > 0) {
+      computerScore++;
+    }
+    console.log(result);
+    alert(`${result}\nYou: ${playerScore}, Comp: ${computerScore}`);
+  }
+};
+
+game();
